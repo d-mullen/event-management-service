a328f4272a9c5378f123ca2cd8fb5cd9f364898d
Cornelius Toole <197863+corntoole@users.noreply.github.com>
2022-09-27 17:45:14 -0500

    test: add coverage for query criteria conversions from protos to domain types

--- pkg/adapters/server/grpc/dto_test.go
+++ pkg/adapters/server/grpc/dto_test.go
@@ -3,9 +3,13 @@ package grpc_test
 import (
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/pkg/errors"
 	"github.com/zenoss/event-management-service/pkg/adapters/server/grpc"
+	eventModel "github.com/zenoss/event-management-service/pkg/models/event"
 	"github.com/zenoss/zing-proto/v11/go/cloud/eventquery"
 	"github.com/zenoss/zing-proto/v11/go/event"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"google.golang.org/protobuf/types/known/structpb"
 )
@@ -147,5 +151,183 @@ var _ = Describe("DTO Tests", func() {
 			Expect(r5).ShouldNot(BeNil())
 			GinkgoWriter.Printf("%+v %+v", r5, r5.PageInput)
 		})
+	})
+	Context("ClauseToProtoEventFilter", func() {
+		DescribeTable("ClauseProtoToEventFilter Table-driven Tests",
+			func(
+				input *eventquery.Clause,
+				expected *eventModel.Filter,
+				expectedErr error,
+			) {
+				actualFilter, actualErr := grpc.ClauseProtoToEventFilter(input)
+				if expectedErr != nil {
+					Expect(actualErr).To(MatchError(expectedErr.Error()))
+				} else {
+					Expect(actualErr).ToNot(HaveOccurred())
+				}
+				if expected != nil {
+					Expect(actualFilter).To(BeEquivalentTo(expected))
+				}
+			},
+			Entry("nil clause",
+				nil,
+				nil,
+				errors.New("nil clause"),
+			),
+			Entry("with-scope filter",
+				&eventquery.Clause{
+					Clause: &eventquery.Clause_WithScope{
+						WithScope: &eventquery.WithScope{
+							Scope: &eventquery.WithScope_EntityScopeCursor{
+								EntityScopeCursor: &eventquery.WithScope_WithEntityScopeCursor{Cursor: "cursor_1"},
+							},
+						},
+					},
+				},
+				&eventModel.Filter{
+					Op:    eventModel.FilterOpScope,
+					Field: "scope",
+					Value: &eventModel.Scope{
+						ScopeType: eventModel.ScopeEntity,
+						Cursor:    "cursor_1",
+					},
+				},
+				nil,
+			),
+			Entry("regex filter",
+				&eventquery.Clause{
+					Clause: &eventquery.Clause_Filter{
+						Filter: &eventquery.Filter{
+							Field:    "summary",
+							Operator: eventquery.Filter_OPERATOR_REGEX,
+							Value:    mustToValue("alarm"),
+						},
+					},
+				},
+				&eventModel.Filter{
+					Op:    eventModel.FilterOpRegex,
+					Field: "summary",
+					Value: "alarm",
+				},
+				nil,
+			),
+			Entry("invalid filter",
+				&eventquery.Clause{
+					Clause: &eventquery.Clause_Filter{
+						Filter: &eventquery.Filter{
+							Field:    "summary",
+							Operator: eventquery.Filter_Operator(9999),
+							Value:    mustToValue("alarm"),
+						},
+					},
+				},
+				nil,
+				status.Error(codes.InvalidArgument, "invalid filter"),
+			),
+			Entry("not clause",
+				&eventquery.Clause{
+					Clause: &eventquery.Clause_Not_{
+						Not: &eventquery.Clause_Not{
+							Clause: &eventquery.Clause{
+								Clause: &eventquery.Clause_Filter{Filter: &eventquery.Filter{
+									Field:    "f1",
+									Operator: eventquery.Filter_OPERATOR_EQUALS,
+									Value:    mustToValue(1),
+								}},
+							},
+						},
+					},
+				},
+				&eventModel.Filter{
+					Op: eventModel.FilterOpNot,
+					Value: &eventModel.Filter{
+						Op:    eventModel.FilterOpEqualTo,
+						Field: "f1",
+						Value: float64(1),
+					},
+				},
+				nil,
+			),
+			Entry("and clause",
+				&eventquery.Clause{
+					Clause: &eventquery.Clause_And_{
+						And: &eventquery.Clause_And{
+							Clauses: []*eventquery.Clause{
+								{
+									Clause: &eventquery.Clause_Filter{Filter: &eventquery.Filter{
+										Field:    "f1",
+										Operator: eventquery.Filter_OPERATOR_EQUALS,
+										Value:    mustToValue(1),
+									}},
+								},
+								{
+									Clause: &eventquery.Clause_Filter{Filter: &eventquery.Filter{
+										Field:    "f2",
+										Operator: eventquery.Filter_OPERATOR_NOT_EQUALS,
+										Value:    mustToValue("foo"),
+									}},
+								},
+							},
+						},
+					},
+				},
+				&eventModel.Filter{
+					Op: eventModel.FilterOpAnd,
+					Value: []*eventModel.Filter{
+						{
+							Op:    eventModel.FilterOpEqualTo,
+							Field: "f1",
+							Value: float64(1),
+						},
+						{
+							Op:    eventModel.FilterOpNotEqualTo,
+							Field: "f2",
+							Value: "foo",
+						},
+					},
+				},
+				nil,
+			),
+			Entry("or clause",
+				&eventquery.Clause{
+					Clause: &eventquery.Clause_Or_{
+						Or: &eventquery.Clause_Or{
+							Clauses: []*eventquery.Clause{
+								{
+									Clause: &eventquery.Clause_Filter{Filter: &eventquery.Filter{
+										Field:    "f1",
+										Operator: eventquery.Filter_OPERATOR_EQUALS,
+										Value:    mustToValue(1),
+									}},
+								},
+								{
+									Clause: &eventquery.Clause_Filter{Filter: &eventquery.Filter{
+										Field:    "f2",
+										Operator: eventquery.Filter_OPERATOR_NOT_EQUALS,
+										Value:    mustToValue("foo"),
+									}},
+								},
+							},
+						},
+					},
+				},
+				&eventModel.Filter{
+					Op: eventModel.FilterOpOr,
+					Value: []*eventModel.Filter{
+						{
+							Op:    eventModel.FilterOpEqualTo,
+							Field: "f1",
+							Value: float64(1),
+						},
+						{
+							Op:    eventModel.FilterOpNotEqualTo,
+							Field: "f2",
+							Value: "foo",
+						},
+					},
+				},
+				nil,
+			),
+		)
 	})
 })
